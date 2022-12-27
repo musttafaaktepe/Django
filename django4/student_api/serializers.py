6defdfd04ed79bbd46620ba074ef209cb36f05e3
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-28 00:21:34 +0300

    django4 v4

--- django4/student_api/serializers.py
+++ django4/student_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Student, Path
 
 # class StudentSerializer(serializers.Serializer):
 #     first_name = serializers.CharField(max_length=50)
@@ -20,15 +20,29 @@ from .models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
     born_year = serializers.SerializerMethodField()
-    # path = serializers.StringRelatedField() # read_only
-    # path_id = serializers.IntegerField()
+    path = serializers.StringRelatedField() # read_only
+    path_id = serializers.IntegerField()
     class Meta:
         model = Student
         # fields = "__all__"
-        fields = ["id","first_name", "last_name","number", "age", "born_year" ]
+        fields = ["id","first_name", "last_name","number", "age", "born_year", "path", "path_id"]
         # exclude = ["number"]
 
     def get_born_year(self, obj):
         import datetime
         current_time = datetime.datetime.now()
         return current_time.year - obj.age
+
+
+class PathSerializer(serializers.ModelSerializer):
+    
+    # students = StudentSerializer(many=True)
+    students = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='detail'
+    )
+    class Meta:
+        model = Path
+        # fields = "__all__"
+        fields = ["id", "path_name", "students"]
