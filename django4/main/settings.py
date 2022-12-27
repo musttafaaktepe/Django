027be455f155eaf8a264b60a736cf95f1ecff764
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-27 23:44:05 +0300

    django4 v3

--- django4/main/settings.py
+++ django4/main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'student_api',
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
--- django4/student_api/urls.py
+++ django4/student_api/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path, include
-from .views import home
+from .views import home , student_api, student_api_get_update_delete
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path("student/", student_api),
+    path("student/<int:pk>", student_api_get_update_delete, name="detail"),
 ]
