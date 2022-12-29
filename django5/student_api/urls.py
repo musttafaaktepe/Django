8fcf5f1cc9deaa33c33be1cf733b1801d8eb1087
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-29 10:13:25 +0300

    django5 v2

--- django5/student_api/urls.py
+++ django5/student_api/urls.py
@@ -1,10 +1,8 @@
 
 from django.urls import path, include
-from .views import home , student_api, student_api_get_update_delete, path_api
+from .views import home 
 
 urlpatterns = [
     path('', home),
-    path("student/", student_api),
-    path("student/<int:pk>", student_api_get_update_delete, name="detail"),
-    path("path/", path_api)
+    
 ]
