be5536a000a76c8c66dca27a772115bfe8bfa9f9
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-29 22:12:05 +0300

    django7 v2

--- django7/todo/urls.py
+++ django7/todo/urls.py
@@ -3,9 +3,21 @@ from rest_framework import routers
 
 from .views import (    
     todo_list_create,   
+    todo_home,
+    todo_detail,
+    Todos,
+    TodoDetail,
+    TodoMVS
 )
 
+router = routers.DefaultRouter()
+router.register('todo', TodoMVS)
+
 urlpatterns = [
-    path('list-create/', todo_list_create),
-    
+    path('', todo_home),
+    # path('list-create/', todo_list_create),
+    # path('detail/<int:id>', todo_detail),
+    path('list-create/', Todos.as_view()),
+    path('detail/<int:pk>', TodoDetail.as_view()),
+    path('', include(router.urls))
 ]
