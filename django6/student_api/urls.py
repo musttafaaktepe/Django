98a789365c12b175b888803d7235f6ee3f754b51
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-29 13:55:53 +0300

    django6 v1

--- django6/student_api/urls.py
+++ django6/student_api/urls.py
@@ -7,8 +7,17 @@ from .views import (
     # student_detail,
     # student_update,
     # student_delete,
-    student_api,
-    student_api_get_update_delete
+    # student_api,
+    # student_api_get_update_delete
+
+    StudentListCreate,
+    StudentDetail,
+    # StudentGAV,
+    # StudentDetailGAV,
+    # StudentCV,
+    # StudentDetailCV
+    # StudentMVS,
+    # PathMVS
     ) 
 
 urlpatterns = [
@@ -18,8 +27,17 @@ urlpatterns = [
     # path("student-detail/<int:pk>/", student_detail, name='detail'),
     # path("student-update/<int:pk>/", student_update, name='update'),
     # path("student-delete/<int:pk>/", student_delete, name='delete'),
-    path('student/', student_api),
-    path('student/<int:pk>', student_api_get_update_delete)
+    # path('student/', student_api),
+    # path('student/<int:pk>', student_api_get_update_delete)
+
+
+    path("student/", StudentListCreate.as_view()),
+    path("student/<int:pk>", StudentDetail.as_view()),
+    # path("student/", StudentGAV.as_view()),
+    # path("student/<int:pk>", StudentDetailGAV.as_view()),
+    # path("student/", StudentCV.as_view()),
+    # path("student/<int:pk>", StudentDetailCV.as_view()),
+    # path("", include(router.urls))
 ]
 
 
