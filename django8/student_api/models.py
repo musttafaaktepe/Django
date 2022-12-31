e6bdfa8e328909acf9830e763288017861c2f193
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-31 10:13:13 +0300

    django8 v4

--- django8/student_api/models.py
+++ django8/student_api/models.py
@@ -11,6 +11,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     number = models.IntegerField(blank=True, null=True)
+    created=models.DateTimeField(auto_now_add=True)
     # age = models.IntegerField(null=True)
 
     def __str__(self):
--- django8/student_api/pagination.py
+++ django8/student_api/pagination.py
@@ -10,3 +10,8 @@ class CustomLimitOffsetPagination(LimitOffsetPagination):
     default_limit=10
     limit_query_param='adet'
     offset_query_param='baslangic'
+
+class CustomCursorPagination(CursorPagination):
+    cursor_query_param='imlec'
+    page_size=10
+    ordering='-id'
