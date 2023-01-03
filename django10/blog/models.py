8108d94edaf498a8e65b32b8b21f8b1795301fd4
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-03 09:55:07 +0300

    django10 v3

--- django10/blog/models.py
+++ django10/blog/models.py
@@ -7,15 +7,11 @@ class Category(models.Model):
         return self.name
 
 class Blog(models.Model):
-    # CHOICES = (
-    #     ('p', 'Published'),
-    #     ('d', 'Draft')
-    # )
+    
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT) # category_id
     is_published = models.BooleanField(default=False)
-    # status = models.CharField(choices=CHOICES, default='d')
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
