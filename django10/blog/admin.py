483ec6b0e88c642eabe1a24205d1f58574032e3b
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-03 13:19:55 +0300

    django10 control

--- django10/blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category, Blog
-
-admin.site.register(Category)
-admin.site.register(Blog)
-# Register your models here.
--- django10/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
-from .models import Category
-
-class CategoryView(ModelViewSet):
-    querset=Category.objects.all()
-    serializer_class=None
