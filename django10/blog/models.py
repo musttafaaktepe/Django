dd91ea48e4b8aabfe11640d9ba3c5d8f8ca8f6e5
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-04 10:08:17 +0300

    django10 blog3

--- django10/blog/models.py
+++ django10/blog/models.py
@@ -6,4 +6,18 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Blog(models.Model):
+    
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT) 
+    is_published = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
 
---
+++ django10/blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+
+from .views import CategoryView, BlogView
+from rest_framework import routers
+
+
