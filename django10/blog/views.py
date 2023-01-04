1732ffba5f52725cb063953fa82e411d37b8fcea
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-04 11:10:34 +0300

    django10 blog5

--- django10/blog/views.py
+++ django10/blog/views.py
@@ -7,9 +7,12 @@ from .serializers import CategorySerializer, BlogSerializer
 class CategoryView(ModelViewSet):  # crud
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filterset_fields = ['name']
+
 
 
 class BlogView(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    filterset_fields = ['category']
     
--- django10/main/settings.py
+++ django10/main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        # 'rest_framework.filters.SearchFilter',
+    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ]
+}
