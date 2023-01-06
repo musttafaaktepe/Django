2e2ccb2358344ecf6c4b8cb0399fc8d3334cd4d7
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-06 09:40:36 +0300

    django13 v5

--- django11-12-13/fapp/flight/permissions.py
+++ django11-12-13/fapp/flight/permissions.py
@@ -1,10 +1,8 @@
 from rest_framework import permissions
 
-class IsStaffforReadOnly(permissions.IsAdminUser):
+class IsStafforReadOnly(permissions.IsAdminUser):
 
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-
         return bool(request.user and request.user.is_staff)
