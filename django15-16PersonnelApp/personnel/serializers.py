091da9a586b0929e876fc8989d4477dc9d0f04db
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-16 18:26:20 +0300

    django16 v2

--- django15-16PersonnelApp/personnel/serializers.py
+++ django15-16PersonnelApp/personnel/serializers.py
@@ -13,3 +13,22 @@ class DepartmentSerializer(serializers.ModelSerializer):
     def get_personnel_count(self, obj):
         return obj.personals.count()
     
+class PersonnelSerializer(serializers.ModelSerializer):
+    
+    days_since_joined = serializers.SerializerMethodField()
+    create_user_id = serializers.IntegerField(required = False)
+    create_user = serializers.StringRelatedField()
+    
+    class Meta:
+        model = Personnel
+        fields = "__all__"
+        
+
+    def create(self, validated_data):
+        validated_data["create_user_id"] = self.context['request'].user.id
+        instance = Personnel.objects.create(**validated_data)
+        return instance
+
+    def get_days_since_joined(self, obj):
+        return (now() - obj.start_date).days
+    
--- django15-16PersonnelApp/personnel/views.py
+++ django15-16PersonnelApp/personnel/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Department, Personnel
+from .serializers import DepartmentSerializer, PersonnelSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsStafforReadOnly
+# , IsOwnerAndStaffOrReadOnly
 
-# Create your views here.
+class DepartmentView(generics.ListCreateAPIView):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+    permission_classes = [IsAuthenticated, IsStafforReadOnly]
+    
+class PersonnelListCreateView(generics.ListCreateAPIView):
+    serializer_class = PersonnelSerializer
+    queryset = Personnel.objects.all()
+    permission_classes = [IsAuthenticated]
