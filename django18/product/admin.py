2d5c7d002aa588fdd52ef8cdb1c938636b32cf01
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-19 21:20:36 +0300

    django18 v2

--- django18/product/admin.py
+++ django18/product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "is_in_stock", "update_date")
+    list_editable = ("is_in_stock", )
+    list_display_links = ("create_date", ) #can't add items in list_editable to here
+    ordering = ("name",)  
+    search_fields = ("name",)
+    prepopulated_fields = {'slug' : ('name',)}   # when adding product in admin site
+    list_per_page = 25
+    date_hierarchy = "update_date"
+
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.site_title = "Clarusway Title"
+admin.site.site_header = "Clarusway Admin Portal"
+admin.site.index_title = "Welcome to Clarusway Admin Portal"
