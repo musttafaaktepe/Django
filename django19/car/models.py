449f45fa63765e0ac4e8f003dd5c7c98b2da3b85
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-26 20:48:47 +0300

    django19 v2

--- django19/car/models.py
+++ django19/car/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    GEAR = (
+        ("a", "Automatic"),
+        ("m", "Manual"),)
+    plate_number = models.CharField(max_length=15, unique=True)
+    brand = models.CharField(max_length=15)
+    model = models.CharField(max_length=20)
+    year = models.SmallIntegerField()
+    gear = models.CharField(max_length=1, choices=GEAR)
+    rent_per_day = models.SmallIntegerField()
+    availability = models.BooleanField(default=True)
+    
+
+    def __str__(self):
+        return f'{self.model} - {self.brand} - {self.plate_number}'
+
+    
--- django19/main/settings.py
+++ django19/main/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'dj_rest_auth',
     'rest_framework.authtoken',
+    'car',
 ]
 
 MIDDLEWARE = [
