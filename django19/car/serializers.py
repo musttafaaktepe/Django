6850685a904fde7c308a7e7baddd21c1406e8cc5
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-01-28 12:29:00 +0300

    django19 v6

--- django19/car/serializers.py
+++ django19/car/serializers.py
@@ -13,3 +13,30 @@ class CarSerializer(serializers.ModelSerializer):
             "rent_per_day",
             "availability"
         )
+
+    def get_fields(self):
+        fields = super().get_fields()
+        request = self.context.get('request')
+
+        if request.user and not request.user.is_staff:
+            fields.pop('availability')
+            fields.pop('plate_number')
+        return fields
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = (
+            'id',
+            'customer',
+            'car',
+            'start_date',
+            'end_date'
+        )
+        # validators = [
+        #     serializers.UniqueTogetherValidator(
+        #         queryset=Reservation.objects.all(),
+        #         fields=('customer', 'start_date', 'end_date'),
+        #         message=('You alreday have a reservation between these dates...')
+            # )
+        # ]
--- django19/car/views.py
+++ django19/car/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from .models import Car, Reservation
-from .serializers import CarSerializer
+from .serializers import CarSerializer, ReservationSerializer
 from .permissions import IsStaffOrReadOnly
 
 from django.db.models import Q
@@ -20,13 +20,22 @@ class CarView(ModelViewSet):
         start = self.request.query_params.get('start')
         end = self.request.query_params.get('end')
 
-        cond1 = Q(start_date__lt=end)
-        cond2 = Q(end_date__gt=start)
+        if start is not None or end is not None:
 
+            # not_available = Reservation.objects.filter(
+            #     start_date__lt=end, end_date__gt=start
+            # ).values_list('car_id', flat=True)  # [1, 2]
 
-        not_available = Reservation.objects.filter(
-            cond1 & cond2 
-        ).values_list('car_id', flat=True)
-        queryset = queryset.exclude(id__in=not_available)
+            not_available = Reservation.objects.filter(
+                Q(start_date__lt=end) & Q(end_date__gt=start)
+            ).values_list('car_id', flat=True)  # [1, 2]
+
+            print(not_available)
+
+            queryset = queryset.exclude(id__in=not_available)
 
         return queryset
+
+class ReservationView(ListCreateAPIView):
+    queryset = Reservation .objects.all()
+    serializer_class = ReservationSerializer
