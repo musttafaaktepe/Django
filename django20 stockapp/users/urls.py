1922d8728d6a3dd0a01231292dbd5de6fad1191c
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-01 20:08:58 +0300

    django20

--- django20 stockapp/users/urls.py
+++ django20 stockapp/users/urls.py
@@ -4,5 +4,5 @@ from .views import RegisterAPI, ProfileUpdateView
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path("register/", RegisterAPI.as_view()),
-    path("profile/<int:pk>/", ProfileUpdateView.as_view()),
+    
 ]
