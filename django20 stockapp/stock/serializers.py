55e904b57640c9967ee6f2596b8d3abd67bb3fc6
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-02 21:37:45 +0300

    django20 -v2

---
+++ django20 stockapp/stock/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Category, Brand, Product, Firm, Purchases, Sales
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ("id", "name")
+
+        
--- django20 stockapp/stock/views.py
+++ django20 stockapp/stock/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Brand, Product, Firm, Purchases, Sales
+from .serializers import CategorySerializer
+
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+
+
+
 
 # Create your views here.
