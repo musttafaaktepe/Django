e22c746b6325552923c4f2d914b4328737390b3c
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-02 22:03:53 +0300

    django20 -v3

--- django20 stockapp/stock/views.py
+++ django20 stockapp/stock/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Category, Brand, Product, Firm, Purchases, Sales
 from .serializers import CategorySerializer
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    
-
-
-
-
-# Create your views here.
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
