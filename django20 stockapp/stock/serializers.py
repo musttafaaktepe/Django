4db06420b9660695b52d3ef3a18308863956fed3
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-03 17:57:22 +0300

    django20 -v4

--- django20 stockapp/stock/serializers.py
+++ django20 stockapp/stock/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import Category, Brand, Product, Firm, Purchases, Sales
 
+
 class CategorySerializer(serializers.ModelSerializer):
     product_count = serializers.SerializerMethodField()  # read_only
     
     class Meta:
         model = Category
-        fields = ("id", "name", "product_count")
+        fields =  "__all__"
         
     def get_product_count(self, obj):
         return Product.objects.filter(category_id=obj.id).count()
@@ -26,3 +27,6 @@ class CategoryProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ("id", "name", "product_count", "products")
+        
+    def get_product_count(self, obj):
+        return Product.objects.filter(category_id=obj.id).count()
