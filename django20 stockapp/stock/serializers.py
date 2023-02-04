2ecff033cc63eb9c00aa967a978fb74ae8f99f1d
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-04 11:20:13 +0300

    django20 -v5

--- django20 stockapp/stock/serializers.py
+++ django20 stockapp/stock/serializers.py
@@ -30,3 +30,25 @@ class CategoryProductSerializer(serializers.ModelSerializer):
         
     def get_product_count(self, obj):
         return Product.objects.filter(category_id=obj.id).count()
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = (
+            'id',
+            'name',
+            'image'
+        )
+
+
+class FirmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Firm
+        fields = (
+            'id',
+            'name',
+            'phone',
+            'image',
+            'address'
+        )
+
--- django20 stockapp/stock/views.py
+++ django20 stockapp/stock/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
 from .models import Category, Brand, Product, Firm, Purchases, Sales
-from .serializers import CategorySerializer, CategoryProductSerializer
+from .serializers import CategorySerializer, CategoryProductSerializer, BrandSerializer, FirmSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import DjangoModelPermissions
 
@@ -17,3 +17,17 @@ class CategoryView(viewsets.ModelViewSet):
         if self.request.query_params.get("name"):
             return CategoryProductSerializer
         return super().get_serializer_class()
+
+class BrandView(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+class FirmView(viewsets.ModelViewSet):
+    queryset = Firm.objects.all()
+    serializer_class = FirmSerializer
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
