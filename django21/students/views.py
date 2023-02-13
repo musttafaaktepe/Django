9a117c480893ef5f13feb3aa37600e5222df489f
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-13 21:15:45 +0300

    django21 v2

--- django21/students/views.py
+++ django21/students/views.py
@@ -5,9 +5,23 @@ from django.http import HttpResponse
 def home(request):
 
     context = {
-        'title': 'Clarusway'
+        'title': 'Clarusway',
+        'desc': 'This is a description',
+        'number': 1111,
+        'list1': ['a', 1, 'b', ['c', 2]],
+        'dict1': {
+            'key1': 'value1',
+            'key2': 'value2'
+        }
+
     }
 
     return render(request, 'students/home.html', context)
 
     # return HttpResponse('<p> Hello </p>')
+
+
+'''
+{{ variables }}
+{% command %}
+'''
