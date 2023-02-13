dc7ec4114aeb881942696ed9f490513d43899750
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2023-02-13 21:35:26 +0300

    django21 v3

--- django21/students/views.py
+++ django21/students/views.py
@@ -5,14 +5,19 @@ from django.http import HttpResponse
 def home(request):
 
     context = {
-        'title': 'Clarusway',
+        'title': 'clarusway',
         'desc': 'This is a description',
         'number': 1111,
         'list1': ['a', 1, 'b', ['c', 2]],
         'dict1': {
             'key1': 'value1',
             'key2': 'value2'
-        }
+        },
+        'dict_list': [
+            {'name': 'zed', 'age': 25},
+            {'name': 'amy', 'age': 21},
+            {'name': 'joe', 'age': 50},
+        ]
 
     }
 
@@ -24,4 +29,5 @@ def home(request):
 '''
 {{ variables }}
 {% command %}
+| ---> filter
 '''
